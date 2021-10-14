a6c03a3958e5ce3d77dffb60a9d97485865ce905
hbisevac1 <hbisevac1@etf.unsa.ba>
2021-10-14 15:55:47 +0200

    Initial commit

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -3,6 +3,7 @@ package ba.unsa.etf.rpr;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Java programiranje");
+
+        System.out.println("C++ je bolji realnou");
     }
 }
